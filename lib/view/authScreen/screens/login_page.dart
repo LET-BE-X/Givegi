172949cfb07c5da7a549765f106bc1ef34102f78
ayushh-gautam <ayushgautam896@gmail.com>
2023-11-06 12:23:39 +0545

    login page done

--- lib/view/authScreen/screens/login_page.dart
+++ lib/view/authScreen/screens/login_page.dart
@@ -1,7 +1,11 @@
 import 'package:ecommercegiveji/utils/color_constant.dart';
 import 'package:ecommercegiveji/view/authScreen/widgets/customText.dart';
+import 'package:ecommercegiveji/view/authScreen/widgets/custom_button.dart';
 import 'package:ecommercegiveji/view/authScreen/widgets/custom_stack.dart';
+import 'package:ecommercegiveji/view/authScreen/widgets/custon_textfield.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -12,10 +16,34 @@ class LoginScreen extends StatelessWidget {
       appBar: AppBar(
         title: customText(color: kWhiteColor, myText: 'Login', size: 20),
       ),
-      body: myStack(child: Column(
-
-        
-      )),
+      body: myStack(
+          child: Column(
+        children: [
+          CustomTextfield(
+            decoration: InputDecoration(
+              hintText: 'xyz@gmail.com',
+            ),
+          ),
+          20.heightBox,
+          CustomTextfield(
+            decoration: InputDecoration(suffixIcon: Icon(Icons.visibility)),
+          ),
+          20.heightBox,
+          MyButton(
+            ontap: () {},
+            height: 50,
+            width: 343,
+            text: 'Login',
+          ),
+          20.heightBox,
+          customText(
+            color: kGreenColor,
+            myText: 'Don\'t have an account?Signup',
+            size: 20,
+            fontWeight: FontWeight.w500,
+          )
+        ],
+      ).marginOnly(left: 30, right: 30, top: 20)),
     );
   }
 }
