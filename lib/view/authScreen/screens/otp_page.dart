b1fbef586147915d203efad52c158f2189011dde
ayushh-gautam <ayushgautam896@gmail.com>
2023-11-07 14:01:21 +0545

    flutter_verification_code package added

--- lib/view/authScreen/screens/otp_page.dart
+++ lib/view/authScreen/screens/otp_page.dart
@@ -1,7 +1,11 @@
 import 'package:ecommercegiveji/utils/color_constant.dart';
+import 'package:ecommercegiveji/utils/image_const.dart';
+import 'package:ecommercegiveji/view/authScreen/widgets/customText.dart';
+import 'package:ecommercegiveji/view/authScreen/widgets/custom_button.dart';
 import 'package:ecommercegiveji/view/authScreen/widgets/custom_stack.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class OtpPage extends StatefulWidget {
   const OtpPage({super.key});
@@ -15,11 +19,34 @@ class _OtpPageState extends State<OtpPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: myStack(
+          topchild: Column(
+            children: [
+              Image.asset(
+                otpImage,
+                height: 174,
+                width: 174,
+              ),
+              10.heightBox,
+              customText(
+                  align: TextAlign.center,
+                  color: kGreenColor,
+                  myText: 'Please enter OTP sent to your mobile number',
+                  fontWeight: FontWeight.w500,
+                  size: 18),
+            ],
+          ).marginOnly(top: 30, left: 30, right: 30),
           x: 0,
           y: 0,
           color: kOtp,
           child: Column(
-            children: [],
+            children: [
+              MyButton(
+                ontap: () {},
+                height: 53.85,
+                width: 343,
+                text: 'Confirm',
+              )
+            ],
           ).marginOnly(left: 50, right: 50, top: 40)),
     );
   }
