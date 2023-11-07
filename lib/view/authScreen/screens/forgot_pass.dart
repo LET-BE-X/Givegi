f9578decd9c8a3234f959fb92390f5f56137cc79
ayushh-gautam <ayushgautam896@gmail.com>
2023-11-07 13:00:31 +0545

    forget pass page completed

--- lib/view/authScreen/screens/forgot_pass.dart
+++ lib/view/authScreen/screens/forgot_pass.dart
@@ -18,19 +18,31 @@ class ForgetPass extends StatefulWidget {
 class _ForgetPassState extends State<ForgetPass> {
   @override
   Widget build(BuildContext context) {
-    return myStack(
-        child: Column(
-      children: [
-        customText(
-            color: kGreenColor,
-            myText:
-                'Please enter your email to receive a\n reset password link',
-            size: 18),
-        50.heightBox,
-        CustomTextfield(
-          decoration: InputDecoration(hintText: 'E-mail Id'),
-        )
-      ],
-    ).marginOnly(left: 40, right: 40, top: 40));
+    return Scaffold(
+      body: myStack(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          customText(
+              align: TextAlign.center,
+              color: kGreenColor,
+              myText:
+                  'Please enter your email to receive a\n reset password link',
+              size: 18),
+          40.heightBox,
+          CustomTextfield(
+            decoration: InputDecoration(hintText: 'E-mail Id'),
+          ),
+          30.heightBox,
+          MyButton(
+              ontap: () {
+                Get.to(() => Home());
+              },
+              height: 55,
+              text: 'Submit',
+              width: 343)
+        ],
+      ).marginOnly(left: 50, right: 50, top: 40)),
+    );
   }
 }
