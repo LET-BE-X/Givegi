3cb320cf56de3b341bcfa0dc800306a498ef6004
ayushh-gautam <ayushgautam896@gmail.com>
2023-11-09 20:04:32 +0545

    minor changes on home screen

--- lib/view/HomeScreen/Widgets/New_Arival.dart
+++ lib/view/HomeScreen/Widgets/New_Arival.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
+import 'package:get/get.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 import '../../../utils/ListConst.dart';
@@ -14,37 +15,56 @@ class NewArival extends StatelessWidget {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
-      child:  Row(
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: List.generate(6, (index) => Card(
-            elevation: 7,
-            child: SizedBox(
-              height: 280.h,
-              width: 220.h,
-              child: Row(
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Center(child: Image.asset(featureproductimage[index],width: 130.h,height: 150.h,fit: BoxFit.fill,)),
-                      Gap(20.h),
-                      featureproductname[index].text.bold.size(22.h).make(),
-                      Gap(10.h),
-                      "Description".text.size(18.h).color(Colors.blueGrey).make(),
-                      Gap(20.h),
-                      featureproductprice[index].text.size(20.h).color(Colors.black87).make(),
-                    ],
-                  ).onTap(() { }),
-                  Gap(30.h),
-
-                ],
-              ),
-            ),
-          ).box.padding( EdgeInsets.only(left: 10.h,top: 10.h)).make()),
-        ),
-
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: List.generate(
+            6,
+            (index) => Card(
+                  elevation: 1,
+                  child: SizedBox(
+                    height: 250.h,
+                    width: 220.h,
+                    child: Row(
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Center(
+                                child: Image.asset(
+                              featureproductimage[index],
+                              width: 130.w,
+                              height: 130.h,
+                              fit: BoxFit.cover,
+                            )),
+                            Gap(20.h),
+                            featureproductname[index]
+                                .text
+                                .bold
+                                .size(22.h)
+                                .make(),
+                            Gap(5.h),
+                            "Description"
+                                .text
+                                .size(18.h)
+                                .color(Colors.blueGrey)
+                                .make(),
+                            Gap(5.h),
+                            featureproductprice[index]
+                                .text
+                                .size(20.h)
+                                .color(Colors.black87)
+                                .make()
+                                .marginOnly(bottom: 10),
+                          ],
+                        ).onTap(() {}),
+                        Gap(10.h),
+                      ],
+                    ),
+                  ),
+                ).box.padding(EdgeInsets.only(left: 10.h, top: 10.h)).make()),
+      ),
     );
   }
 }
