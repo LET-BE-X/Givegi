4318330481b87332df39756cb0400110fa479df3
saroj kumar sah <sarojahah152@gmail.com>
2023-11-15 18:46:39 +0545

    current location change

--- lib/view/ProfileScreen/Screens/NewAddress.dart
+++ lib/view/ProfileScreen/Screens/NewAddress.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
-import 'package:get/get.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:open_street_map_search_and_pick/open_street_map_search_and_pick.dart';
 
 
@@ -19,24 +21,30 @@ class _NewAddressState extends State<NewAddress> {
   @override
   Widget build(BuildContext context) {
 
+
     return Scaffold(
       body:OpenStreetMapSearchAndPick(
-          center: LatLong(26.8302, 86.2666),
+          // center: LatLong(26.8302, 86.2666),
+        onGetCurrentLocationPressed: () async{
+          Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+          return LatLng(position.latitude, position.longitude);
+        },
+
+
           buttonColor: Colors.green,
           hintText: "Search",
           buttonHeight: 40,
           buttonWidth: double.infinity,
           locationPinIconColor: Colors.green,
           buttonText: 'Save Address',
-
-
-
           onPicked: (pickedData) {
             widget.data?.add(pickedData.addressName);
             print(widget.data);
+          },
+        // onGetCurrentLocationPressed: GetcurrentPosition(FutureOr<LatLong>),
 
 
-          }),
+          ),
     );
   }
 }
